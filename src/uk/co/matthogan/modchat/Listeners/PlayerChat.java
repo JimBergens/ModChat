8be918f980bba5f4b676b07ae302ff565925d6dc
Matthew Hogan <matt@matthogan.co.uk>
2014-09-12 23:19:40 +0100

    Initial commit
    Initial commit

---
+++ src/uk/co/matthogan/modchat/Listeners/PlayerChat.java
@@ -0,0 +1,40 @@
+package uk.co.matthogan.modchat.Listeners;
+
+/*
+ * File: PlayerChat.java
+ * CopyRight (C) Matthew Hogan 2014 All rights reserved.
+ * Do not duplicate, publish, modify or otherwise distribute.
+ */
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+/**
+ * @author Matthew Hogan
+ */
+public class PlayerChat implements Listener {
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+
+            if (event.getPlayer().hasPermission("modchat.use") && event.getMessage().startsWith("!")) {
+            event.setCancelled(true);
+            final String message =  ChatColor.DARK_RED + "[" + ChatColor.RED + "ModChat" + ChatColor.DARK_RED + "] "
+                    + ChatColor.GREEN + event.getPlayer().getName() + ": " + event.getMessage().replace("!", "");
+
+            // Send the players with the permission: modchat.use.
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (player.hasPermission("modchat.use")) {
+                    player.sendMessage(message);
+                }
+            }
+
+            // Send the console the same message.
+            Bukkit.getConsoleSender().sendMessage(message);
+        }
+    }
+}
---
+++ src/uk/co/matthogan/modchat/Main.java
@@ -0,0 +1,19 @@
+package uk.co.matthogan.modchat;
+
+/*
+ * File: Main.java
+ * CopyRight (C) Matthew Hogan 2014 All rights reserved.
+ * Do not duplicate, publish, modify or otherwise distribute.
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+import uk.co.matthogan.modchat.Listeners.PlayerChat;
+
+/**
+ * @author Matthew Hogan
+ */
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() { this.getServer().getPluginManager().registerEvents(new PlayerChat(), this); }
+}
